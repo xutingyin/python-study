5f0f97174df9a7bdb0e736e9355298b206f269d5
xuyin <sunshinexuty@163.com>
2020-01-18 23:24:19 +0800

    Python简介

--- cn/xutingyin/HelloPython.py
+++ cn/xutingyin/HelloPython.py
@@ -1,6 +1,10 @@
+# Python 简介
+# 著名的“龟叔”Guido van Rossum在1989年圣诞节期间，为了打发无聊的圣诞节而编写的一个编程语言。
+# 龟叔给Python 的定义就是“优雅”、“明确”、“简单”。
+# Python 的缺点：①运行速度慢 ；②代码不能加密
 print("Hello,Python!")
 # 自定义变量名，无需指定数据类型
-name = 100+200
+name = 100 + 200
 print(name)
 # 输出多个数据，并且还是不同的数据类型
-print("123","hello","jjjj",123*123)
+print("123", "hello", "jjjj", 123 * 123)
